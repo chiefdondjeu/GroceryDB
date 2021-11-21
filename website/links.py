76a68b3ae676c282370a9a84b5a2cae09d5ef88d
Florent Dondjeu Tschoufack <florentdondjeu@yahoo.com>
2021-11-21 08:47:34 -0500

    added last login time

--- website/links.py
+++ website/links.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, url_for, redirect
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 from . import db
 from .models import User
 
@@ -8,12 +9,12 @@ links = Blueprint("links", __name__)
 
 @links.route("/")
 def home():
-	return render_template("home.html", user=current_user)
+	return render_template("home.html", current_user=current_user)
 
-@links.route("/admin")
+@links.route("/dashboard")
 @login_required
-def admin():
-	return render_template("admin.html", user=current_user)
+def dashboard():
+	return render_template("dashboard.html", current_user=current_user)
 
 @links.route("/login", methods=["GET", "POST"])
 def login():
@@ -24,14 +25,17 @@ def login():
 		user = User.query.filter_by(username=username).first()
 		if user:
 			if check_password_hash(user.password, password):
-				flash("Login successfull.", category="success")
+				flash(f"Login successfull. Last login {user.last_login}", category="success")
+				current_time = datetime.now().replace(microsecond=0)
+				user.last_login = current_time
+				db.session.commit()
 				login_user(user, remember=True)
-				return redirect(url_for('links.admin'))
+				return redirect(url_for('links.dashboard'))
 			else:
 				flash("Incorrect password or username.", category="error")
 		else:
 			flash(f"{username} does not exist!", category="error")
-	return render_template("login.html", user=current_user)
+	return render_template("login.html", current_user=current_user)
 
 @links.route('/logout')
 @login_required
